d6c8e6715cba36fc0c055a0514cd8407112aeb9e
Ahmed Gamal <agamal0077@gmail.com>
2024-02-09 12:43:15 +0200

    re roll all 10 die when clicking the roll button

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import { useState } from 'react'
 import Die from './Components/Die'
 import './App.css'
 
+/**
+ * Challenge: Create a `Roll Dice` button that will re-roll
+ * all 10 dice
+ *
+ * Clicking the button should generate a new array of numbers
+ * and set the `dice` state to that new array (thus re-rendering
+ * the array to the page)
+ */
+
 function App() {
   const [Dice, setDice] = useState(randomNewDice())
 
@@ -14,14 +23,21 @@ function App() {
     return numbers
   }
 
-  const diceElements = Dice.map((die) => (
-    <Die value={die} />
+  const diceElements = Dice.map((die, index) => (
+    <Die key={index} value={die} />
   ))
+
+  function rollDice() {
+    setDice(randomNewDice)
+  }
 
   return (
     <>
       <main>
         <div className="die-container">{diceElements}</div>
+        <button className="roll-button" onClick={rollDice}>
+          Roll Dice
+        </button>
       </main>
     </>
   )
